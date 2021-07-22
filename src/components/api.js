c184fe12f96c478049f9202f5f259ae9d77f724c
lauro <lauropimentel12@gmail.com>
2021-07-21 22:56:46 -0300

    Frontend heroku-api update

--- src/components/api.js
+++ src/components/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Pode ser algum servidor executando localmente:
 // http://localhost:3000
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://websysm.herokuapp.com",
   headers: { "Content-Type": "application/json" },
 });
 
